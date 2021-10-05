ddf6f2eecba3e1d25b6eee4b2b63c032e6e7851d
siba98 <gyansiba.patro@gmail.com>
2021-10-05 23:01:41 +0530

    Siba:[Added] REGEX Pattern for checking Mobile Number

---
+++ UC4_MobileNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day19_REGEX_Assignments
+{
+    class UC4_MobileNumber
+    {
+        public static string Regex_MobileNumber = "^[0-9]{2}[ ][0-9]{10}$";
+
+        public bool ValidatePhoneNumber(string phonenumber)
+        {
+            return Regex.IsMatch(phonenumber, Regex_MobileNumber);
+        }
+    }
+}
