c098a05ad6405759befa10f6573794cd368ba437
siba98 <gyansiba.patro@gmail.com>
2021-10-06 22:27:39 +0530

    Siba:[Modified] REGEX Pattern for checking User's Password

--- UC5_6_7_8_Password.cs
+++ UC5_6_7_8_Password.cs
@@ -7,8 +7,8 @@ namespace Day19_REGEX_Assignments
 {
     class UC5_6_7_8_Password
     {
-        public static string Regex_PSWD = "^([$#.%+@-~!]{1}([a-zA-Z0-9]*[A-Z]{1,}[0-9]{1,})){8,}$";
-
+        //public static string Regex_PSWD = "^([$#.%+@-~!]{1}([a-zA-Z0-9]*[A-Z]{1,}[0-9]{1,})){8,}$";
+        public static string Regex_PSWD = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=-])[a-zA-Z0-9!@#$%^&*()_+=-]{8,}$";
         public bool ValidatePassword(string password)
         {
             return Regex.IsMatch(password, Regex_PSWD);
