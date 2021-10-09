e60c40e130b764a3309917b76e83867c38407c8d
siba98 <gyansiba.patro@gmail.com>
2021-10-09 20:47:49 +0530

    Siba:[Modified] Main Method

--- Program.cs
+++ Program.cs
@@ -8,42 +8,42 @@ namespace Day19_REGEX_Assignments
         {
             Console.WriteLine("**********REGULAR EXPRESSION**********\n\n");
 
-            //UC1_FirstName pattern1 = new UC1_FirstName();
-            //Console.WriteLine(pattern1.ValidateFirstName("Siba"));
-            //Console.WriteLine(pattern1.ValidateFirstName("siba"));
+            UC1_FirstName pattern1 = new UC1_FirstName();
+            Console.WriteLine(pattern1.ValidateFirstName("Siba"));
+            Console.WriteLine(pattern1.ValidateFirstName("siba"));
 
 
 
 
-            //UC2_Lastname pattern2 = new UC2_Lastname();
-            //Console.WriteLine(pattern2.ValidateLastName("Patro"));
+            UC2_Lastname pattern2 = new UC2_Lastname();
+            Console.WriteLine(pattern2.ValidateLastName("Patro"));
 
 
 
 
-            //#region UC3 and UC9
-            //UC3_9_Email pattern3 = new UC3_9_Email();
-            //Console.WriteLine(pattern3.ValidateEmail("abc.xyz@bl.co.in"));
-            //Console.WriteLine(pattern3.ValidateEmail("abc-100@yahoo.com"));
-            //Console.WriteLine(pattern3.ValidateEmail("abc..2002@gmail.com"));
-            //Console.WriteLine(pattern3.ValidateEmail("abc()*@gmail.com"));
-            //Console.WriteLine(pattern3.ValidateEmail("abc+100@gmail.com"));
-            //Console.WriteLine(pattern3.ValidateEmail("abc@1.com"));
-            //Console.WriteLine(pattern3.ValidateEmail("abc.100@abc.com.au"));
-            //Console.WriteLine(pattern3.ValidateEmail("abc111@abc.com"));
-            //Console.WriteLine(pattern3.ValidateEmail("abc-100@abc.net"));
-            //Console.WriteLine(pattern3.ValidateEmail("abc@gmail.com.1a"));
-            //Console.WriteLine(pattern3.ValidateEmail("abc@%*.com"));
-            //Console.WriteLine(pattern3.ValidateEmail("abc@abc@gmail.com"));
-            //#endregion
+            #region UC3 and UC9
+            UC3_9_Email pattern3 = new UC3_9_Email();
+            Console.WriteLine(pattern3.ValidateEmail("abc.xyz@bl.co.in"));
+            Console.WriteLine(pattern3.ValidateEmail("abc-100@yahoo.com"));
+            Console.WriteLine(pattern3.ValidateEmail("abc..2002@gmail.com"));
+            Console.WriteLine(pattern3.ValidateEmail("abc()*@gmail.com"));
+            Console.WriteLine(pattern3.ValidateEmail("abc+100@gmail.com"));
+            Console.WriteLine(pattern3.ValidateEmail("abc@1.com"));
+            Console.WriteLine(pattern3.ValidateEmail("abc.100@abc.com.au"));
+            Console.WriteLine(pattern3.ValidateEmail("abc111@abc.com"));
+            Console.WriteLine(pattern3.ValidateEmail("abc-100@abc.net"));
+            Console.WriteLine(pattern3.ValidateEmail("abc@gmail.com.1a"));
+            Console.WriteLine(pattern3.ValidateEmail("abc@%*.com"));
+            Console.WriteLine(pattern3.ValidateEmail("abc@abc@gmail.com"));
+            #endregion
 
 
 
 
-            //UC4_MobileNumber pattern4 = new UC4_MobileNumber();
-            //Console.WriteLine(pattern4.ValidatePhoneNumber("91 7504832115"));
-            //Console.WriteLine(pattern4.ValidatePhoneNumber("1441 7504832115")); //Bermuda
-            //Console.WriteLine(pattern4.ValidatePhoneNumber("91 750483211511"));
+            UC4_MobileNumber pattern4 = new UC4_MobileNumber();
+            Console.WriteLine(pattern4.ValidatePhoneNumber("91 7504832115"));
+            Console.WriteLine(pattern4.ValidatePhoneNumber("1441 7504832115")); //Bermuda
+            Console.WriteLine(pattern4.ValidatePhoneNumber("91 750483211511"));
 
 
 
